0a52ea6556f17f76c35ad52c32e350915804fffa
Jonathan Wald <jwald@jwald.com>
2011-11-09 15:56:47 -0600

    Adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+
+#include "main-window.h"
+
+int main(int argc, char *argv[])
+{
+  Q_INIT_RESOURCE(application);
+
+  QApplication app(argc, argv);
+  app.setOrganizationName("C0ntag10n");
+  app.setApplicationName("Software Authentication Cracker");
+  //specify a  new font.
+  QFont newFont("Impact", 12, QFont::Bold);
+  //set font of application
+  QApplication::setFont(newFont);
+  MainWindow mainWin;
+  mainWin.show();
+  return app.exec();
+}

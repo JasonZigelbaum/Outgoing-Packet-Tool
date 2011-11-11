9112e4610d08960ea9fa645eaf533ee0341f605f
Jason Zigelbaum <jasonzigelbaum@gmail.com>
2011-11-11 14:30:34 -0600

    added route editor class

---
+++ route-editor.h
@@ -0,0 +1,15 @@
+#ifndef ROUTE_EDITOR_H
+#define ROUTE_EDITOR_H
+#endif
+
+using namespace std;
+	
+class RouteEditor
+{
+public:
+	bool addRouteEntry(std::string ip);
+	bool deleteRouteEntry(std::string ip);
+
+private:
+	/* data */
+};
